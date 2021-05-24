46898cc76641881fed1534e37f9cd3ca0d1a78e0
Tomasz Karwowski <tkarwowski@internships.antmicro.com>
2021-05-24 11:13:29 +0200

    echo working, all other commands give not found message

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+// use std::collections::HashMap;
+// use std::collections::hash_map::RandomState;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -13,12 +15,23 @@ extern {
     fn alert(s: &str);
 }
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, msh!");
-}
+// static COMMANDS: HashMap<String, Box<dyn FnOnce(Vec<String>) -> String>, RandomState> = vec![
+//     ("echo".to_owned(), Box::new(echo)),
+// ].into_iter().collect();
 
 #[wasm_bindgen]
 pub fn handle_line(line: String) -> String {
-    format!("# msh returns\r\n{}", line)
+    let mut words = line.split_whitespace();
+    let command_name = words.next().unwrap();
+    let args = words.map(|arg| arg.to_owned()).collect();
+    match command_name {
+        "echo" => echo(args),
+        name => format!("command '{}' not found", name)
+    }
+    // format!("# msh returns\r\n{}", COMMANDS[command_name](args))
 }
+
+fn echo(args: Vec<String>) -> String {
+    args.join(" ")
+}
+
