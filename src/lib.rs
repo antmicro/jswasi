7fa78695d38d025e1b019e9f5082216f6da8d348
Tomasz Karwowski <tkarwowski@internships.antmicro.com>
2021-06-23 18:40:02 +0200

    two way communication

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,17 @@ use std::fs;
 //         web_sys::console::log_1(&format!( $( $t )* ).into());
 //     }
 // }
-
+// #[link(wasm_import_module = "env")]
 extern {
-    fn alert(s: &str);
+    fn logHello();
 }
 
+// one of these two must be here
 #[export_name = "stdin"]
+// #[no_mangle]
 pub extern fn stdin(code: i32) -> i32 {
-    code
+    unsafe { logHello(); };
+    code + 1
     // parse input
     // let mut words = line.split_whitespace();
     // let command_name = words.next().unwrap();
@@ -30,36 +33,36 @@ pub extern fn stdin(code: i32) -> i32 {
     // // return result
     // result.unwrap_or_else(|error| format!("error occurred: {}", error))
 }
-
-fn echo(args: Vec<String>) -> Result<String, Box<dyn std::error::Error>> {
-    Ok(args.join(" "))
-}
-
-fn cat(mut args: Vec<String>) -> Result<String, Box<dyn std::error::Error>> {
-    let filename = args.remove(0);
-    let content = fs::read_to_string(&filename).unwrap_or_else(|e|
-        unsafe {
-            alert(&format!("failed reading file: {}", e));
-            "".to_owned()
-        }
-    );
-    Ok(content)
-}
-
-fn save(mut args: Vec<String>) -> Result<String, Box<dyn std::error::Error>> {
-    let filename = args.remove(0);
-    fs::write(filename, args.join("")).unwrap_or_else(|e|
-        unsafe {
-            alert(&format!("failed writing to file: {}", e));
-        }
-    );
-    Ok("".to_owned())
-}
-
-fn ls(mut args: Vec<String>) -> Result<String, Box<dyn std::error::Error>> {
-    let output = String::new();
-    // if args.len() == 1 {
-    //     let
-    // }
-    Ok(output)
-}
+//
+// fn echo(args: Vec<String>) -> Result<String, Box<dyn std::error::Error>> {
+//     Ok(args.join(" "))
+// }
+//
+// fn cat(mut args: Vec<String>) -> Result<String, Box<dyn std::error::Error>> {
+//     let filename = args.remove(0);
+//     let content = fs::read_to_string(&filename).unwrap_or_else(|e|
+//         unsafe {
+//             alert(&format!("failed reading file: {}", e));
+//             "".to_owned()
+//         }
+//     );
+//     Ok(content)
+// }
+//
+// fn save(mut args: Vec<String>) -> Result<String, Box<dyn std::error::Error>> {
+//     let filename = args.remove(0);
+//     fs::write(filename, args.join("")).unwrap_or_else(|e|
+//         unsafe {
+//             alert(&format!("failed writing to file: {}", e));
+//         }
+//     );
+//     Ok("".to_owned())
+// }
+//
+// fn ls(mut args: Vec<String>) -> Result<String, Box<dyn std::error::Error>> {
+//     let output = String::new();
+//     // if args.len() == 1 {
+//     //     let
+//     // }
+//     Ok(output)
+// }
