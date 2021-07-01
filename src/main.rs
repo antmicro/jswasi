da0b78792634e39c596be33db787df057d4f9379
Peter Gielda <pgielda@antmicro.com>
2021-07-02 01:47:33 +0200

    add one try

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,8 @@ fn main() {
         //thread::sleep(delay);
     }
 
-    println!("Type something (3 tries):");
-    for _ in 0..3 {
+    println!("Type something (4 tries):");
+    for _ in 0..4 {
        let mut buff = String::new();
        io::stdin().read_line(&mut buff);
        println!("You entered: {}", buff);
