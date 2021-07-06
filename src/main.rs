b74c99b2809cdcc869e2163d648f7ca056b15025
Tomasz Karwowski <tkarwowski@internships.antmicro.com>
2021-07-06 12:16:27 +0200

    simplest example of fs access that fails

--- src/main.rs
+++ src/main.rs
@@ -10,38 +10,12 @@ fn main() {
         println!("Iteration -- Hello from Rust/WASM!");
     }
 
-    // let mut file = File::create("test.txt").unwrap_or_else(|e| {
-    //     println!("error occurred: {}", e);
-    //     exit(1);
-    // });
+    let _file = File::create("test.txt").unwrap_or_else(|e| {
+        println!("error occurred: {}", e);
+        exit(1);
+    });
 
-    // let mut file = File::open("/tmp/test.txt").unwrap_or_else(|e| {
-    //     println!("error occurred: {}", e);
-    //     exit(1);
-    // });
-
-    // fs::write("test.txt", "saved contents").unwrap_or_else(|e| {
-    //     println!("error occurred: {}", e);
-    // });
-    // let buffer = fs::read_to_string("hello.rs").unwrap_or_else(|e| {
-    //     println!("error occurred: {}", e);
-    //     "".to_owned()
-    // });
-    // println!("buffer: {}", buffer);
-
-    // println!("Type something (4 tries):");
-    // for _ in 0..4 {
-    //    let mut buff = String::new();
-    //    io::stdin().read_line(&mut buff);
-    //    println!("You entered: {}", buff);
-    // }
-    
     println!("I regret to announce this is the end!");
-
-    // // This wouldn't work, hands whole site, doesn't even render terminal
-    // loop {
-    //     println!("Hello from infinite loop!")
-    // }
 
     return;
 }
