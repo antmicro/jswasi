b36f60e08fd6276620a31d682bf812245fc9c70b
Tomasz Karwowski <tkarwowski@internships.antmicro.com>
2021-07-08 15:18:17 +0200

    shell implementation with working backspace handling

--- src/bin/shell.rs
+++ src/bin/shell.rs
@@ -1,16 +1,45 @@
 use std::io;
-use std::io::Write;
+use std::io::{Write, Read};
+use std::str;
 
 fn main() {
+    let mut input = String::new();
+
     loop {
+        // prompt for input
         print!("$ ");
         io::stdout().flush();
 
-        let mut input = String::new();
-        io::stdin().read_line(&mut input);
+        let mut c = [0];
+        // read line
+        loop {
+            io::stdin().read_exact(&mut c);
+            match c[0] {
+                // enter
+                10 => break,
+                // CR
+                127 => {
+                    if !input.is_empty() {
+                        input.remove(input.len() - 1);
+                        print!("{} {}", 8 as char, 8 as char);
+                    }
+                },
+                // control codes
+                code if code < 32 => {
 
-        let command = input.trim();
+                },
+                // regular characters
+                _ => {
+                    input.push(c[0] as char);
+                    print!("{}", c[0] as char);
+                    // print!("({},{})", c[0] as u8, c[0] as char);
+                }
+            }
+            io::stdout().flush();
+        }
 
-        print!("entered: {}\n", command);
+        // handle line
+        print!("\nentered: {}\n", input);
+        input.clear();
     }
 }
