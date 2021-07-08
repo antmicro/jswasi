42a00d1594128a8647046e4e6e37f1d66921577d
Peter Gielda <pgielda@antmicro.com>
2021-07-08 19:42:58 +0200

    Only display debug messages when debug == true

--- node.js
+++ node.js
@@ -7,9 +7,11 @@ if (process.argv.length < 3) {
   process.exit(1);
 }
 
+let debug = false;
+
 let workers = [];
 workers[0] = { id: 0, worker: new Worker('./worker.js') };
-workers[1] = { id: 1, worker: new Worker('./worker.js') };
+if (debug) workers[1] = { id: 1, worker: new Worker('./worker.js') };
 
 let terminated = false;
 let buffer = "";
@@ -21,10 +23,10 @@ let ev = (event) => {
                 const lck = new Int32Array(event.data[2], 0, 1);
                 const len = new Int32Array(event.data[2], 4, 1);
                 if (buffer.length != 0) {
-                    console.log("got buffer request of len "+len[0]+", notifying");
+                    if (debug) console.log("got buffer request of len "+len[0]+", notifying");
                     const sbuf = new Uint16Array(event.data[2], 8, len[0]);
                     len[0] = (buffer.length > len[0]) ? len[0] : buffer.length;
-                    console.log("current buffer is " + buffer + ", copying len " + len[0]);
+                    if (debug) console.log("current buffer is " + buffer + ", copying len " + len[0]);
                     for (let j = 0; j < len[0]; j++) {
                         sbuf[j] = buffer.charCodeAt(j);
                     }
@@ -39,24 +41,24 @@ let ev = (event) => {
             } else if (action === "stderr") {
                 console.log(event.data[2]);
             } else if (action === "exit") {
-                console.log("We got exit command, result = " + event.data[2]);
+                if (debug) console.log("We got exit command, result = " + event.data[2]);
                 workers[event.data[0]].worker.terminate();
                 terminated = true;
             } else if (action === "console") {
-                console.log("WORKER "+event.data[0]+": " + event.data[2]);
+                if (debug) console.log("WORKER "+event.data[0]+": " + event.data[2]);
             }
         }
 
 
 workers[0].worker.on('message', ev);
-workers[1].worker.on('message', ev);
+if (debug) workers[1].worker.on('message', ev);
 
 //myWorker.onmessage = ev;
 
-console.log('sending message!');
+if (debug) console.log('sending message!');
 workers[0].worker.postMessage(["start", process.argv[2], 0]);
-workers[1].worker.postMessage(["start", process.argv[2], 1]);
-console.log('message sent!');
+if (debug) workers[1].worker.postMessage(["start", process.argv[2], 1]);
+if (debug) console.log('message sent!');
 
 function heartbeat() {
      console.log("bip");
