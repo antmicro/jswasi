7b7a2ddd6f765b27aeb0e0c551dad1b211cd920e
Tomasz Karwowski <tkarwowski@internships.antmicro.com>
2021-07-21 14:41:22 +0200

    cd and pwd builtins

--- src/bin/shell.rs
+++ src/bin/shell.rs
@@ -1,9 +1,11 @@
-use std::io;
-use std::io::{Write, Read};
 use std::fs::File;
+use std::io;
+use std::io::{Read, Write};
 use std::process::exit;
+use std::path::Path;
 
 fn main() {
+    let mut pwd = "/".to_owned();
     let mut input = String::new();
 
     loop {
@@ -17,18 +19,21 @@ fn main() {
             io::stdin().read_exact(&mut c).unwrap();
             match c[0] {
                 // enter
-                10 => break,
+                10 => {
+                    println!();
+                    break;
+                }
                 // backspace
                 127 => {
                     if !input.is_empty() {
                         input.remove(input.len() - 1);
                         print!("{} {}", 8 as char, 8 as char); // '\b \b', clear left of cursor
                     }
-                },
+                }
                 // control codes
                 code if code < 32 => {
                     // ignore for now
-                },
+                }
                 // regular characters
                 _ => {
                     input.push(c[0] as char);
@@ -46,17 +51,34 @@ fn main() {
 
         match command {
             // built in commands
-            "echo" => println!("\n{}", args.join(" ")),
-            "cd" => println!("\ncd not yet implemented"),
+            "echo" => println!("{}", args.join(" ")),
+            "cd" => {
+                if args.is_empty() {
+                    pwd = "/".to_owned();
+                } else {
+                    let path = args[0];
+
+                    let new_pwd = if path.starts_with("/") {
+                        path.to_owned()
+                    } else {
+                        format!("{}{}", pwd, path)
+                    };
+
+                    // // simply including this in source breaks shell
+                    // if !Path::new(&new_pwd).exists() {
+                    //     println!("cd: no such file or directory: {}", new_pwd);
+                    // }
+                }
+            },
+            "pwd" => println!("{}", pwd),
             "exit" => exit(0),
             // external commands
             "duk" | "main" | "shell" => {
-                println!();
                 File::open(format!("!{}", command));
-            },
+            }
             // edge cases
-            "" => println!(),
-            _ => println!("\ncommand not found: {}", command),
+            "" => {}
+            _ => println!("command not found: {}", command),
         }
         input.clear();
     }
