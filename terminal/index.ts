e6c659ae8a191c166adc139a5aa568995907c2b4
Tomasz Karwowski <tkarwowski@internships.antmicro.com>
2021-08-11 10:15:59 +0200

    refactor index.ts, introduce WorkerTable class

--- terminal/index.ts
+++ terminal/index.ts
@@ -1,6 +1,19 @@
 import {hterm, lib} from "./hterm-all.js";
+import {WorkerTable} from "./worker-table.js";
 
 let buffer = "";
+
+function send_buffer_to_worker(lck: Int32Array, len: Int32Array, sbuf: Uint8Array) {
+    console.log("got buffer request of len " + len[0] + ", notifying");
+    len[0] = (buffer.length > len[0]) ? len[0] : buffer.length;
+    console.log("current buffer is '" + buffer + "', copying len " + len[0]);
+    for (let j = 0; j < len[0]; j++) {
+        sbuf[j] = buffer.charCodeAt(j);
+    }
+    buffer = buffer.slice(len[0]);
+    Atomics.store(lck, 0, 1);
+    Atomics.notify(lck, 0);
+}
 
 async function init_all() {
     // If you are a cross-browser web app and want to use window.localStorage.
@@ -12,32 +25,8 @@ async function init_all() {
     // const ant = await home.getDirectoryHandle("ant", {create: true});
     // const history = await ant.getFileHandle(".shell_history", {create: true});
 
-    let workers = [];
-    workers[0] = {id: 0, worker: new Worker('worker.js', {type: "module"}), buffer_request_queue: []};
-    let current_worker = 0;
-
-    function send_buffer_to_worker(lck: Int32Array, len: Int32Array, sbuf: Uint8Array) {
-        console.log("got buffer request of len " + len[0] + ", notifying");
-        len[0] = (buffer.length > len[0]) ? len[0] : buffer.length;
-        console.log("current buffer is '" + buffer + "', copying len " + len[0]);
-        for (let j = 0; j < len[0]; j++) {
-            sbuf[j] = buffer.charCodeAt(j);
-        }
-        buffer = buffer.slice(len[0]);
-        Atomics.store(lck, 0, 1);
-        Atomics.notify(lck, 0);
-    }
-
-    function handle_program_end(worker) {
-        worker.worker.terminate();
-        // notify parent that they can resume operation
-        Atomics.store(worker.parent_lck, 0, 1);
-        Atomics.notify(worker.parent_lck, 0);
-        // remove worker from workers array
-        workers.splice(worker.id, 1);
-        current_worker -= 1; // TODO: workers stack/tree
-        console.log(`Awaiting input from WORKER ${current_worker}`);
-    }
+    let workerTable = new WorkerTable;
+    workerTable.spawnWorker(null, null);
 
     const setupHterm = () => {
         const t = new hterm.Terminal("profile-id");
@@ -49,30 +38,24 @@ async function init_all() {
                 let code = data.charCodeAt(0);
                 console.log(data, code);
 
-                const worker = workers[current_worker];
                 if (code !== 13 && code < 32) {
                     // control characters
                     if (code == 3) {
-                        console.log(`got ^C control, killing current worker (${worker.id})`);
-                        handle_program_end(worker);
+                        workerTable.terminateWorker(workerTable.currentWorker);
                     } else if (code == 4) {
-                        console.log(`got ^D, releasing buffer read lock (if present) with value -1`);
-                        if (worker.buffer_request_queue.length !== 0) {
-                            const lck = worker.buffer_request_queue[0].lck;
-                            Atomics.store(lck, 0, -1);
-                            Atomics.notify(lck, 0);
-                        }
+                        workerTable.releaseWorker(workerTable.currentWorker, -1);
                     }
                 } else {
                     // regular characters
                     buffer = buffer + data;
+
                     // echo
                     t.io.print(data);
 
                     // each worker has a buffer request queue to store fd_reads on stdin that couldn't be handled straight away
                     // now that buffer was filled, look if there are pending buffer requests from current foreground worker
-                    while (workers[current_worker].buffer_request_queue.length !== 0 && buffer.length !== 0) {
-                        let {lck, len, sbuf} = workers[current_worker].buffer_request_queue.shift();
+                    while (workerTable._workerInfos[workerTable.currentWorker].buffer_request_queue.length !== 0 && buffer.length !== 0) {
+                        let {lck, len, sbuf} = workerTable._workerInfos[workerTable.currentWorker].buffer_request_queue.shift();
                         send_buffer_to_worker(lck, len, sbuf);
                     }
                 }
@@ -93,58 +76,63 @@ async function init_all() {
     await lib.init();
     const terminal = setupHterm();
 
-    const worker_onmessage = async (event) => {
+    const on_worker_message = async (event) => {
         const [worker_id, action, data] = event.data;
-        if (action === "buffer") {
-            const lck = new Int32Array(data, 0, 1);
-            const len = new Int32Array(data, 4, 1);
-            const sbuf = new Uint8Array(data, 8, len[0]);
-            if (buffer.length !== 0) {
-                // handle buffer request straight away
-                send_buffer_to_worker(lck, len, sbuf);
-            } else {
-                // push handle buffer request to queue
-                workers[worker_id].buffer_request_queue.push({lck: lck, len: len, sbuf: sbuf});
-            }
-        } else if (action === "stdout") {
-            // let output = new TextDecoder().decode(data).replace("\n", "\n\r");
-            let output = data.replaceAll("\n", "\n\r");
-            terminal.io.print(output);
-        } else if (action === "stderr") {
-            console.log(`STDERR ${worker_id}: ${data}`);
-        } else if (action === "console") {
-            console.log("WORKER " + worker_id + ": " + data);
-        } else if (action === "exit") {
-            let worker = workers[worker_id];
-            handle_program_end(worker);
-            console.log(`WORKER ${worker_id} exited with result code: ${data}`);
-        } else if (action === "spawn") {
-            const [command, args, env, lck_sbuf] = data;
-            const parent_lck = new Int32Array(lck_sbuf, 0, 1);
-            const id = workers.length;
-            if (command === "mount") {
-                // special case for mount command
-                const mount = await showDirectoryPicker();
-                Atomics.store(parent_lck, 0, 1);
-                Atomics.notify(parent_lck, 0)
-            } else {
-                workers.push({id: id, worker: new Worker("worker.js", {type: "module"}), buffer_request_queue: [], parent_lck});
-                workers[id].worker.onmessage = worker_onmessage;
-                workers[id].worker.postMessage(["start", `${command}.wasm`, id, args, env]);
-                console.log("WORKER " + worker_id + " spawned: " + command);
-                current_worker = id;
-            }
-        } else if (action === "file-access") {
+        switch (action) {
+            case "buffer":
+                const lck = new Int32Array(data, 0, 1);
+                const len = new Int32Array(data, 4, 1);
+                const sbuf = new Uint8Array(data, 8, len[0]);
+                if (buffer.length !== 0) {
+                    // handle buffer request straight away
+                    send_buffer_to_worker(lck, len, sbuf);
+                } else {
+                    // push handle buffer request to queue
+                    workerTable._workerInfos[worker_id].buffer_request_queue.push({lck: lck, len: len, sbuf: sbuf});
+                }
+                break;
+            case "stdout":
+                // let output = new TextDecoder().decode(data).replace("\n", "\n\r");
+                let output = data.replaceAll("\n", "\n\r");
+                terminal.io.print(output);
+                break;
+            case "stderr":
+                console.log(`STDERR ${worker_id}: ${data}`);
+                break;
+            case "console":
+                console.log("WORKER " + worker_id + ": " + data);
+                break;
+            case "exit":
+                workerTable.terminateWorker(worker_id);
+                console.log(`WORKER ${worker_id} exited with result code: ${data}`);
+                break;
+            case "spawn":
+                const [command, args, env, lck_sbuf] = data;
+                const parent_lck = new Int32Array(lck_sbuf, 0, 1);
+                if (command === "mount") {
+                    // special case for mount command
+                    const mount = await showDirectoryPicker();
+                    workerTable.releaseWorker(worker_id, 1);
+                } else {
+                    const id = workerTable.spawnWorker(worker_id, parent_lck);
+                    workerTable.setOnMessage(id, on_worker_message);
+                    workerTable.postMessage(id, ["start", `${command}.wasm`, id, args, env]);
+                    console.log("WORKER " + worker_id + " spawned: " + command);
+                }
+                break;
+            case "fd_read":
 
         }
     }
 
-    workers[0].worker.onmessage = worker_onmessage;
-    workers[0].worker.postMessage(["start", "shell.wasm", 0, [], {
+    workerTable.setOnMessage(0, on_worker_message);
+    workerTable.postMessage(0, ["start", "shell.wasm", 0, [], {
         RUST_BACKTRACE: "full",
         PATH: "/usr/bin:/usr/local/bin",
         X: "3"
-    }], null);
+    }]);
+
+    console.log("init_all done")
 }
 
 window.onload = init_all;
---
+++ terminal/worker-table.ts
@@ -0,0 +1,116 @@
+import {WASI_ESUCCESS} from "./constants.js";
+
+// class Stdin {
+//     read(len) {
+//         if (len === 0) return [new Uint8Array([]), 0];
+//         const buf = new SharedArrayBuffer((len * 2) + 8); // lock, len, data
+//         const lck = new Int32Array(buf, 0, 1);
+//         const request_len = new Int32Array(buf, 4, 1);
+//         request_len[0] = len;
+//         // send buffer read request to main thread
+//         // it can either be handled straight away or queued for later
+//         // either way we block with Atomics.wait until buffer is filled
+//         worker_send(["buffer", buf]);
+//         Atomics.wait(lck, 0, 0);
+//         if (Atomics.load(lck, 0) === -1) {
+//             return [new Uint8Array, -1];
+//         }
+//         const sbuf = new Uint8Array(buf, 8, request_len[0]);
+//         BUFFER = BUFFER + String.fromCharCode.apply(null, new Uint8Array(sbuf));
+//         let data = BUFFER.slice(0, len).replace("\r", "\n");
+//         BUFFER = BUFFER.slice(len, BUFFER.length);
+//         return [new TextEncoder().encode(data), 0];
+//     }
+// }
+//
+// class Stdout {
+//     write(content) {
+//         worker_send(["stdout", content]);
+//         return WASI_ESUCCESS;
+//     }
+// }
+//
+// class Stderr {
+//     write(content) {
+//         worker_send(["stderr", content]);
+//         return WASI_ESUCCESS;
+//     }
+// }
+//
+// let fds = [
+//     new Stdin(), // 0
+//     new Stdout(), // 1
+//     new Stderr(), // 2
+//     // new OpenDirectory("/", await navigator.storage.getDirectory()),
+//     // new PreopenDirectory(".", {
+//     //     "hello.rs": new File(new TextEncoder().encode(`fn main() { println!("Hello World!"); }`)),
+//     // }), // 3
+//     // new PreopenDirectory("/tmp", {"test.txt": new File(new TextEncoder().encode("test string content"))}), // 4
+// ];
+
+class WorkerInfo {
+    public id: number;
+    public worker: Worker;
+    public parent_id: number;
+    public parent_lock: Int32Array;
+    public buffer_request_queue: { lck: Int32Array, len: Int32Array, sbuf: Uint8Array }[] = [];
+    public fds = [
+        // new Stdin(), // 0
+        // new Stdout(), // 1
+        // new Stderr(), //
+    ];
+
+    constructor(id: number, worker: Worker, parent_id: number, parent_lock: Int32Array) {
+        this.id = id;
+        this.worker = worker;
+        this.parent_id = parent_id;
+        this.parent_lock = parent_lock;
+    }
+}
+
+export class WorkerTable {
+    public currentWorker = null;
+    private _nextWorkerId = 0;
+    private _workerInfos: Record<number, WorkerInfo> = {};
+
+    spawnWorker(parent_id: number, parent_lock: Int32Array): number {
+        const id = this._nextWorkerId;
+        this.currentWorker = id;
+        this._nextWorkerId += 1;
+        let worker = new Worker("worker.js", {type: "module"});
+        this._workerInfos[id] = new WorkerInfo(id, worker, parent_id, parent_lock);
+        return id;
+    }
+
+    setOnMessage(id: number, onmessage) {
+        this._workerInfos[id].worker.onmessage = onmessage;
+    }
+
+    postMessage(id: number, message) {
+        this._workerInfos[id].worker.postMessage(message);
+        console.log(`message posted to worker ${id}`)
+    }
+
+    terminateWorker(id: number) {
+        console.log(`got ^C control, killing current worker (${id})`);
+        const worker = this._workerInfos[id];
+        worker.worker.terminate();
+        // notify parent that they can resume operation
+        Atomics.store(worker.parent_lock, 0, 1);
+        Atomics.notify(worker.parent_lock, 0);
+        this.currentWorker -= worker.parent_id;
+        // remove worker from workers array
+        delete this._workerInfos[id];
+        console.log(`Awaiting input from WORKER ${this.currentWorker}`)
+    }
+
+    releaseWorker(id: number, lock_value) {
+        console.log(`got ^D, releasing buffer read lock (if present) with value -1`);
+        const worker = this._workerInfos[id];
+        if (worker.buffer_request_queue.length !== 0) {
+            const lck = worker.buffer_request_queue[0].lck;
+            Atomics.store(lck, 0, lock_value);
+            Atomics.notify(lck, 0);
+        }
+    }
+}
