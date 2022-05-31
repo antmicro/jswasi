1fea4ef94c7fa23c994f7adcfcbcbb530368bc24
Grzegorz Placzek <gplaczek@antmicro.com>
2022-05-31 14:45:52 +0200

    tree.rs: fix working directory

--- shell/src/bin/tree.rs
+++ shell/src/bin/tree.rs
@@ -18,6 +18,13 @@ fn visit_dirs(dir: &Path, current_path: &PathBuf, cb: &dyn Fn(&PathBuf, &DirEntr
 }
 
 fn main() -> io::Result<()> {
+
+    #[cfg(target_os = "wasi")]
+    if let Ok(path) = env::var("PWD"){
+        env::set_current_dir(path).unwrap_or_else(|e| {
+            eprintln!("Could not set current working dir: {}", e);
+        });
+    }
     let dir = env::args().nth(1).unwrap_or_else(|| String::from("."));
     visit_dirs(Path::new(&dir), &PathBuf::new(), &|current_path: &PathBuf, entry: &DirEntry| {
         println!("{}", current_path.join(entry.file_name()).display());
