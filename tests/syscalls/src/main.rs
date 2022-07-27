5c3f880a38a94444277a7a4fc5493ecdbf097e06
Grzegorz Placzek <gplaczek@antmicro.com>
2022-07-27 10:45:19 +0200

    Refactor test running in main function

--- tests/syscalls/src/main.rs
+++ tests/syscalls/src/main.rs
@@ -1,13 +1,35 @@
 mod syscalls;
+use std::io::{ Error, ErrorKind, Result };
 use syscalls::*;
 
-fn main() -> std::io::Result<()>{
-    println!("[TEST] environ_sizes_get: {:?}", environ_sizes_get::test_environ_sizes_get());
-    println!("[TEST] args_sizes_get: {:?}", args_sizes_get::test_args_sizes_get());
-    println!("[TEST] fd_prestat_get: {:?}", fd_prestat_get::test_fd_prestat_get());
-    println!("[TEST] fd_fdstat_get: {:?}", fd_fdstat_get::test_fd_fdstat_get());
-    println!("[TEST] fd_filestat_get: {:?}", fd_filestat_get::test_fd_filestat_get());
-    println!("[TEST] fd_read: {:?}", fd_read::test_fd_read());
-    println!("[TEST] fd_write: {:?}", fd_write::test_fd_write());
-    Ok(())
+fn main() -> Result<()>{
+    let tests: Vec<(&str, fn() -> Result<()>)> = vec![
+        ("environ_sizes_get", environ_sizes_get::test_environ_sizes_get as fn() -> Result<()>),
+        ("args_sizes_get", args_sizes_get::test_args_sizes_get as fn() -> Result<()>),
+        ("fd_prestat_get", fd_prestat_get::test_fd_prestat_get as fn() -> Result<()>),
+        ("fd_fdstat_get", fd_fdstat_get::test_fd_fdstat_get as fn() -> Result<()>),
+        ("fd_filestat_get", fd_filestat_get::test_fd_filestat_get as fn() -> Result<()>),
+        ("fd_read", fd_read::test_fd_read as fn() -> Result<()>),
+        ("fd_write", fd_write::test_fd_write as fn() -> Result<()>),
+    ];
+
+    let mut fails: u32 = 0;
+    for (name, test) in &tests {
+        let result = test();
+        if let Err(_) = result { fails += 1; }
+        println!("[TEST] {}: {:?}", name, result);
+    }
+
+    unsafe {
+        // teardown test environment
+        wasi::path_unlink_file(4, "text").unwrap();
+        wasi::path_unlink_file(4, "link").unwrap();
+        wasi::path_remove_directory(4, "tmp").unwrap();
+    }
+    println!("[SUMMARY]: {} tests succeeded, {} tests failed",  tests.len() as u32 - fails, fails);
+    if fails == 0 {
+        Ok(())
+    } else {
+        Err(Error::new(ErrorKind::Other, "Tests failed"))
+    }
 }
