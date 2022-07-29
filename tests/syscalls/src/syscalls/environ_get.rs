0006e3d4f96125cc5e6feac53afb6bf8273fc965
Grzegorz Placzek <gplaczek@antmicro.com>
2022-07-29 14:05:48 +0200

    Add environ_get tests

---
+++ tests/syscalls/src/syscalls/environ_get.rs
@@ -0,0 +1,34 @@
+use std::io::{Error, ErrorKind};
+
+pub fn test_environ_get() -> std::io::Result<()> {
+    unsafe {
+        let (envc, envv_s) = match wasi::environ_sizes_get() {
+            Ok(e) => e,
+            Err(e) => { return Err(Error::new(ErrorKind::Other, e)); }
+        };
+        let mut envs_bufs: Vec<*mut u8> = vec![std::ptr::null_mut(); envc];
+        let mut envv_buf: Vec<u8> = vec![0; envv_s];
+        match wasi::environ_get(envs_bufs.as_mut_ptr(), envv_buf.as_mut_ptr()) {
+            Ok(_) => {
+                // all variables should be delimited by \0 char
+                // here, we check if char before every env var (except the first one) is \0
+                if envs_bufs.len() > 0 {
+                    for i in &envs_bufs[1..] {
+                        let delim = *((*i as usize - 1) as *mut u8);
+                        if delim != 0 {
+                            return Err(Error::new(ErrorKind::Other,
+                                format!(
+                                    "In environ_get(): wrong variable delimiter (expected {}, got {})",
+                                    0, delim)));
+                        }
+                    }
+                    Ok(())
+                } else {
+                    // fail test if no environment variables provided
+                    Err(Error::new(ErrorKind::Other, "No test environment variables provided"))
+                }
+            }
+            Err(e) => { Err(Error::new(ErrorKind::Other, e)) }
+        }
+    }
+}
--- tests/syscalls/src/syscalls/mod.rs
+++ tests/syscalls/src/syscalls/mod.rs
@@ -8,3 +8,4 @@ pub mod fd_filestat_get;
 pub mod fd_read;
 pub mod fd_write;
 pub mod fd_prestat_dir_name;
+pub mod environ_get;
