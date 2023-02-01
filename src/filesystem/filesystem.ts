0cb73627db544548619ca2741c7de42fcb8ae929
Grzegorz Placzek <gplaczek@antmicro.com>
2023-02-01 11:55:54 +0100

    [#40278] Document Descriptor interface

--- src/filesystem/filesystem.ts
+++ src/filesystem/filesystem.ts
@@ -42,48 +42,156 @@ export type Dirent = {
 };
 
 export interface Descriptor {
+  /*
+   * Returns descriptor fdstat structutre
+   */
   getFdstat(): Promise<Fdstat>;
+
+  /*
+   * Returns descriptor filestat structutre
+   */
   getFilestat(): Promise<Filestat>;
+
+  /*
+   * Initializes the descriptor using async functions that cannot be executed in the constructor
+   * @param path - path of a file associated with the descriptor
+   */
   initialize(path: string): Promise<void>;
 
+  /*
+   * Getter for descriptor path
+   */
+  getPath(): string;
+
+  /*
+   * Sets times associated with the file
+   *
+   * @param atim - access time
+   * @param mtim - modification time
+   * @param fstflags - settings
+   *
+   * @returns status code
+   *
+   * @see https://github.com/WebAssembly/WASI/blob/main/phases/snapshot/docs.md#-fstflags-record
+   */
   setFilestatTimes(
     fstflags: Fstflags,
     atim: Timestamp,
     mtim: Timestamp
   ): Promise<number>;
+
+  /*
+   * Sets fdflags for the descriptor
+   * @param flags - flags to set
+   *
+   * @returns status code
+   *
+   * @see https://github.com/WebAssembly/WASI/blob/main/phases/snapshot/docs.md#-fdflags-record
+   */
   setFdstatFlags(flags: Fdflags): Promise<number>;
+
+  /*
+   * Sets rights for the descriptor
+   * @param rightsBase - base rights
+   * @param rightsInheriting - inheriting rights
+   *
+   * @returns status code
+   *
+   * @see https://github.com/WebAssembly/WASI/blob/main/phases/snapshot/docs.md#-rights-record
+   */
   setFdstatRights(
     rightsBase: Rights,
     rightsInheriting: Rights
   ): Promise<number>;
+
+  /*
+   * closes the descriptor
+   */
   close(): Promise<number>;
 
+  /*
+   * Reads contents of the underlying file
+   *
+   * @param len - number of bytes to read
+   *
+   * @returns an object holding
+   * err - error code
+   * buffer - ArrayBuffer with read data
+   */
   read(len: number): Promise<{ err: number; buffer: ArrayBuffer }>;
+
   /*
    * Auxiliary function for internal purposes when we
    * are certain that content of the file is utf-8 text and
    * is relatively short
    */
   read_str(): Promise<{ err: number; content: string }>;
+
+  /*
+   * Read contents of the underlying file at a given position, ignoring the file cursor
+   *
+   * @param len - number of bytes to read
+   * @param pos - position from where to start reading
+   *
+   * @returns an object holding:
+   * err - status code
+   * buffer - ArrayBuffer with read data
+   */
   pread(
     len: number,
     pos: bigint
   ): Promise<{ err: number; buffer: ArrayBuffer }>;
 
+  /*
+   * Writes data to the underlying file
+   *
+   * @param buffer - data to write
+   *
+   * @returns an object holding:
+   * err - error code
+   * written - number of succesfully written bytes
+   */
   write(buffer: DataView): Promise<{ err: number; written: bigint }>;
+
+  /*
+   * Writes data to the underlying file at a given position, ignoring the file cursor
+   *
+   * @param buffer - data to write
+   * @param offset - position from where to start writing
+   *
+   * @returns an object holding:
+   * err - error code
+   * written - number of succesfully written bytes
+   */
   pwrite(
     buffer: DataView,
     offset: bigint
   ): Promise<{ err: number; written: bigint }>;
 
+  /*
+   * Moves the file cursor to the demanded position
+   *
+   * @param offset - number of bytes to move the cursor
+   * @param whence - a position from where to move the cursor
+   *
+   * @returns an object holding:
+   * err - status code
+   * offset - number of bytes succesfully moved
+   */
   seek(
     offset: bigint,
     whence: Whence
-  ): Promise<{
-    err: number;
-    offset: bigint;
-  }>;
+  ): Promise<{ err: number; offset: bigint }>;
 
+  /*
+   * Lists the contents of the underlying directory
+   *
+   * @param refresh - boolean, if set, it refreshes the underlying list of entries
+   *
+   * @returns an object holding:
+   * err - status code
+   * dirents - an array holding dirent structures for the directory contents
+   */
   readdir(refresh: boolean): Promise<{ err: number; dirents: Dirent[] }>;
 }
 
