e9f8547886badb4fb6f38bb79a782c3720fa8399
Krzysztof Juszczyk <kjuszczyk@antmicro.com>
2023-02-20 11:36:51 +0100

    Add `[` symlink to `coreutils`

--- src/terminal.ts
+++ src/terminal.ts
@@ -317,6 +317,7 @@ async function initFs(openedRootDir: OpenDirectory) {
     openedRootDir.addSymlink("/usr/bin/printenv", "/usr/bin/coreutils"),
     openedRootDir.addSymlink("/usr/bin/md5sum", "/usr/bin/coreutils"),
     openedRootDir.addSymlink("/usr/bin/test", "/usr/bin/coreutils"),
+    openedRootDir.addSymlink("/usr/bin/[", "/usr/bin/coreutils"),
     openedRootDir.addSymlink("/usr/bin/wc", "/usr/bin/coreutils"),
     openedRootDir.addSymlink("/usr/bin/true", "/usr/bin/coreutils"),
     openedRootDir.addSymlink("/usr/bin/false", "/usr/bin/coreutils"),
