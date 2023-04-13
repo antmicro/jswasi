5d2dd38c55deb6b895bd6ec1b51a67b5080e3c20
Grzegorz Placzek <gplaczek@antmicro.com>
2023-04-13 15:17:34 +0200

    [#43280] Draft VirtualFilesystem using js-virtualfs

--- src/filesystem/virtual-filesystem.ts
+++ src/filesystem/virtual-filesystem.ts
@@ -12,33 +12,156 @@ import {
   LookupFlags,
 } from "./filesystem.js";
 import * as constants from "../constants.js";
+// @ts-ignore
+import * as vfs from "../vendor/vfs.js";
+import { dirname, basename } from "../utils.js";
 
-class VirtualFilesystem implements Filesystem {
-  private filesMap: Record<string, VirtualFilesystemEntry>;
-}
-
-abstract class VirtualFilesystemEntry {
-  abstract open(fdflags: Fdflags): Promise<VirtualFilesystemDescriptor>;
-
-  constructor(public filestat: Filestat) {}
-}
-
-class VirtualFilesystemFileEntry extends VirtualFilesystemEntry {
-  arrayBuffer: ArrayBuffer;
-  override async open(fdflags: Fdflags): Promise<VirtualFilesystemDescriptor> {
-    return new VirtualFilesystemFileDescriptor(fdflags);
+function wasiFiletype(stat: vfs.Stat): number {
+  switch (stat.mode & vfs.constants.S_IFMT) {
+    case vfs.constants.S_IFREG:
+      return constants.WASI_FILETYPE_REGULAR_FILE;
+    case vfs.constants.S_IFDIR:
+      return constants.WASI_FILETYPE_DIRECTORY;
+    case vfs.constants.S_IFBLK:
+      return constants.WASI_FILETYPE_BLOCK_DEVICE;
+    case vfs.constants.S_IFCHR:
+      return constants.WASI_FILETYPE_CHARACTER_DEVICE;
+    case vfs.constants.S_IFLNK:
+      return constants.WASI_FILETYPE_SYMBOLIC_LINK;
+    case vfs.constants.S_IFSOCK:
+      // Posix doesn't include two filetypes for datagram and stream sockets
+      return constants.WASI_FILETYPE_SOCKET_STREAM;
+    default:
+      return constants.WASI_FILETYPE_UNKNOWN;
   }
 }
 
-class VirtualFilesystemFileEntry extends VirtualFilesystemEntry {
-  entries: Record<string, VirtualFilesystemEntry>;
-  override async open(fdflags: Fdflags): Promise<VirtualFilesystemDescriptor> {
-    return new VirtualFilesystemDirectoryDescriptor(fdflags);
+class VirtualFilesystem implements Filesystem {
+  private virtualFs: vfs.VirtualFS;
+
+  constructor() {
+    const __devMgr = vfs.DeviceManager();
+    const __inoMgr = vfs.InodeManager(__devMgr);
+    const [_, __rootDirIno] = __inoMgr.createINode(vfs.Directory, {});
+    const __fdMgr = vfs.FileDescriptorManager();
+    this.virtualFs = vfs.VirtualFS(
+      0o022,
+      __rootDirIno,
+      __devMgr,
+      __inoMgr,
+      __fdMgr
+    );
+  }
+
+  async mkdirat(desc: Descriptor, path: string): Promise<number> {
+    if (desc instanceof VirtualFilesystemDirectoryDescriptor) {
+      return constants.WASI_EINVAL;
+    }
+
+    const __desc = desc as VirtualFilesystemDirectoryDescriptor;
+    let __path = `${__desc.localPath}/${path}`;
+
+    try {
+      this.virtualFs.mkdirSync(__path);
+    } catch (e: vfs.VirtualFSError) {
+      // TODO: check if these errno numbers are the same as WASI ones
+      return e.errno;
+    }
+
+    return constants.WASI_ESUCCESS;
+  }
+
+  async getFilestat(
+    path: string
+  ): Promise<{ err: number; filestat: Filestat }> {
+    try {
+      const __stat = this.virtualFs.statSync(path);
+      return {
+        err: constants.WASI_ESUCCESS,
+        filestat: {
+          dev: __stat.dev,
+          ino: __stat.ino,
+          nlink: __stat.nlink,
+          filetype: wasiFiletype(__stat),
+          size: __stat.size,
+          atim: __stat.atime,
+          mtim: __stat.mtime,
+          ctim: __stat.ctime,
+        },
+      };
+    } catch (e: vfs.VirtualFSError) {
+      return e.errno;
+    }
+  }
+
+  async unlinkat(
+    desc: Descriptor,
+    path: string,
+    is_dir: boolean
+  ): Promise<number> {
+    if (desc instanceof VirtualFilesystemDirectoryDescriptor) {
+      return constants.WASI_EINVAL;
+    }
+
+    const __desc = desc as VirtualFilesystemDirectoryDescriptor;
+    let __path = `${__desc.localPath}/${path}`;
+
+    try {
+      if (is_dir) {
+        this.virtualFs.unlinkSync(__path);
+      } else {
+        this.virtualFs.rmdirSync(__path);
+      }
+      return constants.WASI_ESUCCESS;
+    } catch (e: vfs.VirtualFSError) {
+      return e.errno;
+    }
+  }
+
+  /*
+   * Map WASI oflags to POSIX ones
+   */
+  private static __posixOflags(oflags: OpenFlags): number {
+    let __oflags = vfs.constants.O_RDWR | vfs.constants.O_NOFOLLOW;
+
+    if (oflags & constants.WASI_O_CREAT) {
+      __oflags |= vfs.constants.O_CREAT;
+    }
+    if (oflags & constants.WASI_O_DIRECTORY) {
+      __oflags |= vfs.constants.O_DIRECTORY;
+    }
+    if (oflags & constants.WASI_O_EXCL) {
+      __oflags |= vfs.constants.O_EXCL;
+    }
+    if (oflags & constants.WASI_O_TRUNC) {
+      __oflags |= vfs.constants.O_TRUNC;
+    } else {
+      __oflags |= vfs.constants.O_APPEND;
+    }
+    return __oflags;
+  }
+
+  async open(
+    path: string,
+    dirflags: LookupFlags,
+    oflags: OpenFlags,
+    fs_rights_base: Rights,
+    fs_rights_inheriting: Rights,
+    fdflags: Fdflags
+  ): Promise<{ err: number; index: number; desc: Descriptor }> {
+    let __oflags = VirtualFilesystem.__posixOflags(oflags);
+    try {
+      vfs.openSync(path, __oflags, 0o777);
+    } catch (e: vfs.VirtualFSError) {
+      // 51 means ELOOP, it is returned when symlink is encountered
+      if (e.errno === 51) {
+      }
+    }
   }
 }
 
 abstract class VirtualFilesystemDescriptor implements Descriptor {
-  protected abstract entry: VirtualFilesystemEntry;
+  protected abstract ino: vfs.Inode;
   protected fdstat: Fdstat;
   protected path: string;
 
@@ -46,13 +169,13 @@ abstract class VirtualFilesystemDescriptor implements Descriptor {
     fs_flags: Fdflags,
     fs_rights_base: Rights,
     fs_rights_inheriting: Rights,
-    entry: VirtualFilesystemEntry
+    ino: vfs.INode
   ) {
     this.fdstat = {
       fs_flags,
       fs_rights_base,
       fs_rights_inheriting,
-      fs_filetype: entry.filestat.filetype,
+      fs_filetype: wasiFiletype(ino.getMetadata()),
     };
   }
 
@@ -69,13 +192,7 @@ abstract class VirtualFilesystemDescriptor implements Descriptor {
   }
 
   async getFilestat(): Promise<Filestat> {
-    return this.entry.filestat;
-  }
-
-  async setFilestatTimes(atim: Timestamp, mtim: Timestamp): Promise<number> {
-    if (atim !== undefined) this.entry.filestat.atim = atim;
-    if (mtim !== undefined) this.entry.filestat.mtim = mtim;
-    return constants.WASI_ESUCCESS;
+    return this.ino.stat();
   }
 
   async setFdstatRights(rights_b: Rights, rights_i: Rights): Promise<number> {
@@ -97,6 +214,7 @@ abstract class VirtualFilesystemDescriptor implements Descriptor {
     return false;
   }
 
+  abstract setFilestatTimes(atim: Timestamp, mtim: Timestamp): Promise<number>;
   abstract read(len: number): Promise<{ err: number; buffer: ArrayBuffer }>;
   abstract read_str(): Promise<{ err: number; content: string }>;
   abstract arrayBuffer(): Promise<{ err: number; buffer: ArrayBuffer }>;
@@ -129,16 +247,16 @@ class VirtualFilesystemFileDescriptor extends VirtualFilesystemDescriptor {
     fs_flags: Fdflags,
     fs_rights_base: Rights,
     fs_rights_inheriting: Rights,
-    override entry: VirtualFilesystemFileEntry
+    override ino: vfs.File
   ) {
-    super(fs_flags, fs_rights_base, fs_rights_inheriting, entry);
+    super(fs_flags, fs_rights_base, fs_rights_inheriting, ino);
     this.cursor = 0;
   }
 
   override async arrayBuffer(): Promise<{ err: number; buffer: ArrayBuffer }> {
     return {
       err: constants.WASI_ESUCCESS,
-      buffer: this.entry.arrayBuffer,
+      buffer: this.ino.data,
     };
   }
 
@@ -176,4 +294,9 @@ class VirtualFilesystemFileDescriptor extends VirtualFilesystemDescriptor {
   ): Promise<{ err: number; written: bigint }> {
     let uint8view = new Uint8Array(this.entry.arrayBuffer);
   }
+}
+
+class VirtualFilesystemDirectoryDescriptor extends VirtualFilesystemDescriptor {
+  private dir: vfs.Directory;
+  public localPath: string;
 }
