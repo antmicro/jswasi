e2cf652cb06b7e123fae2ea6a59561b2e8f36fc8
Grzegorz Placzek <gplaczek@antmicro.com>
2024-02-06 12:52:11 +0100

    [#53127] Add tests for mkdirat and getFilestat

--- tests/unit/fsa.test.ts
+++ tests/unit/fsa.test.ts
@@ -20,7 +20,6 @@ class MockError extends DOMException {
 
 describe("Test fsa filesystem open", () => {
   let fsaFilesystem = new FsaFilesystem();
-  // let navigatorDirectoryGetter;
 
   beforeEach(() => {
     jest.spyOn(fsaUtils, "mapErr").mockImplementation(
@@ -125,5 +124,148 @@ describe("Test fsa filesystem open", () => {
 
     const openErr = await fsaFilesystem.open(fileName, 0, constants.WASI_O_CREAT, 0n, 0n, 0, 0);
     expect(openErr.err).toBe(constants.WASI_EISDIR);
+  });
+});
+
+describe("Test fsa filesystem mkdirat", () => {
+  let fsaFilesystem = new FsaFilesystem();
+
+  beforeEach(() => {
+    jest.spyOn(fsaUtils, "mapErr").mockImplementation(
+      (e: DOMException) => { return (e as MockError).errno; });
+  });
+  afterEach(() => { jest.restoreAllMocks(); });
+
+  test("Directories should be creatable", async () => {
+    const dirName = "directory";
+
+    // @ts-ignore
+    jest.spyOn(fsaUtils, "getTopLevelHandle").mockReturnValue(Promise.resolve({
+      getFileHandle: (_a: string, _b: Object) => { throw new MockError(constants.WASI_ENOENT); },
+      // @ts-ignore
+      getDirectoryHandle: (_a: string, b: { create?: boolean }) => {
+        if (b !== undefined) {
+          if (b.create === true)
+            return Promise.resolve({});
+        }
+        throw new MockError(constants.WASI_ENOENT);
+      }
+    }));
+
+    await fsaFilesystem.initialize({
+      prompt: "false",
+      name: "test",
+    });
+
+    const mkdiratErr = await fsaFilesystem.mkdirat(undefined, dirName);
+    expect(mkdiratErr).toBe(constants.WASI_ESUCCESS);
+  });
+
+  test("Directory creation should fail if a directory exists", async () => {
+    const fileName = "dirname";
+
+    // @ts-ignore
+    jest.spyOn(fsaUtils, "getTopLevelHandle").mockReturnValue(Promise.resolve({
+      getFileHandle: (_a: string, _b: Object) => { throw new MockError(constants.WASI_EISDIR); },
+      // @ts-ignore
+      getDirectoryHandle: (_a: string, _b: Object) => Promise.resolve({})
+    }));
+
+    await fsaFilesystem.initialize({
+      prompt: "false",
+      name: "test",
+    });
+
+    const mkdiratErr = await fsaFilesystem.mkdirat(undefined, fileName);
+    expect(mkdiratErr).toBe(constants.WASI_EEXIST);
+  });
+
+  test("Directory creation should fail if a file exists", async () => {
+    const fileName = "dirname";
+
+    // @ts-ignore
+    jest.spyOn(fsaUtils, "getTopLevelHandle").mockReturnValue(Promise.resolve({
+      // @ts-ignore
+      getFileHandle: (_a: string, _b: Object) => Promise.resolve({}),
+      getDirectoryHandle: (_a: string, _b: Object) => { throw new MockError(constants.WASI_ENOTDIR); },
+    }));
+
+    await fsaFilesystem.initialize({
+      prompt: "false",
+      name: "test",
+    });
+
+    const mkdiratErr = await fsaFilesystem.mkdirat(undefined, fileName);
+    expect(mkdiratErr).toBe(constants.WASI_ENOTDIR);
+  });
+});
+
+describe("Test fsa filesystem getFilestat", () => {
+  let fsaFilesystem = new FsaFilesystem();
+
+  beforeEach(() => {
+    jest.spyOn(fsaUtils, "mapErr").mockImplementation(
+      (e: DOMException) => { return (e as MockError).errno; });
+  });
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.restoreAllMocks();
+  });
+
+  test("Filestat data should be returned for files", async () => {
+    const fileName = "dirname";
+
+    jest.spyOn(metadata, "getStoredData").mockReturnValue(Promise.resolve({
+      dev: 0n,
+      ino: 0n,
+      filetype: constants.WASI_FILETYPE_REGULAR_FILE,
+      nlink: 1n,
+      size: 0n,
+      mtim: 0n,
+      atim: 0n,
+      ctim: 0n,
+    }));
+
+    await fsaFilesystem.initialize({
+      prompt: "false",
+      name: "test",
+      keepMetadata: "true",
+    });
+
+    const getFilestatErr = await fsaFilesystem.getFilestat(fileName);
+    expect(getFilestatErr.err).toBe(constants.WASI_ESUCCESS);
+    expect(getFilestatErr.filestat.filetype).toBe(constants.WASI_FILETYPE_REGULAR_FILE);
+  });
+
+  test("Filestat data should not be returned if file doesn't exist", async () => {
+    const fileName = "dirname";
+
+    jest.spyOn(metadata, "getStoredData").mockReturnValue(Promise.resolve(undefined));
+
+    await fsaFilesystem.initialize({
+      prompt: "false",
+      name: "test",
+      keepMetadata: "true",
+    });
+
+    const getFilestatErr = await fsaFilesystem.getFilestat(fileName);
+    expect(getFilestatErr.err).toBe(constants.WASI_ENOENT);
+  });
+
+  test("Indexeddb should not be searched if keepMetadata is disabled", async () => {
+    const fileName = "dirname";
+
+    // @ts-ignore
+    jest.spyOn(metadata, "getStoredData").mockReturnValue(undefined);
+
+    await fsaFilesystem.initialize({
+      prompt: "false",
+      name: "test",
+      keepMetadata: "false",
+    });
+
+    const getFilestatErr = await fsaFilesystem.getFilestat(fileName);
+    expect(getFilestatErr.err).toBe(constants.WASI_ESUCCESS);
+    expect(metadata.getStoredData).toBeCalledTimes(0);
   });
 });
