0b1c6eb7d2219e31c34917d8c70eee2bf778cedb
Grzegorz Placzek <gplaczek@antmicro.com>
2024-02-19 13:37:05 +0100

    [#42406] Add ioctl interface for reading http status code and headers

--- src/filesystem/virtual-filesystem/wget-device.ts
+++ src/filesystem/virtual-filesystem/wget-device.ts
@@ -8,6 +8,11 @@ import { AbstractVirtualDeviceDescriptor, DeviceFilesystem } from "./device-file
 import { DeviceDriver, major } from "./driver-manager.js";
 import { VirtualFilesystemDescriptor } from "./virtual-filesystem.js";
 
+export const enum ioctlRequests {
+  WGETGS = 0,  // Get status
+  WGETRH = 1,  // Read headers
+  WGETRB = 2,  // Read body
+}
 
 export class WgetDeviceDriver implements DeviceDriver {
   private responses: Record<number, Response>;
@@ -124,12 +129,20 @@ class WgetDevice
   }
 }
 
+const enum bufferType {
+  HEADERS,
+  BODY
+}
+
 class WgetDataDevice
   extends AbstractVirtualDeviceDescriptor
   implements VirtualFilesystemDescriptor
 {
-  private data: ArrayBuffer;
-  private cursor: number;
+  private currentBuffer: bufferType;
+  private body: ArrayBuffer;
+  private headers: ArrayBuffer;
+  private hCursor: number;
+  private bCursor: number;
 
   constructor(
     fs_flags: Fdflags,
@@ -140,18 +153,31 @@ class WgetDataDevice
     private invalidate: () => Promise<number>
   ) {
     super(fs_flags, fs_rights_base, fs_rights_inheriting, ino);
-    this.cursor = 0;
+    this.currentBuffer = bufferType.BODY;
+    this.hCursor = 0;
+    this.bCursor = 0;
+    let __headers = []
+    for (const [key, val] of this.response.headers.entries()) {
+      __headers.push(`${key}: ${val}`);
+    }
+    this.headers = new TextEncoder().encode(__headers.join('\n'));
   }
   isatty(): boolean { return false; }
 
   override async read(len: number, _workerId?: number): Promise<{ err: number; buffer: ArrayBuffer }> {
-    // TODO: This should use readable stream so that the entire body does
-    // not have to be saved in memory before it can be read
-    if (this.data === undefined)
-      this.data = await this.response.arrayBuffer();
+    let buffer;
+    if (this.currentBuffer === bufferType.BODY) {
+      // TODO: This should use readable stream so that the entire body does
+      // not have to be saved in memory before it can be read
+      if (this.body === undefined)
+        this.body = await this.response.arrayBuffer();
 
-    const buffer = this.data.slice(this.cursor, this.cursor + len);
-    this.cursor += buffer.byteLength;
+      buffer = this.body.slice(this.bCursor, this.bCursor + len);
+      this.bCursor += buffer.byteLength;
+    } else {
+      buffer = this.headers.slice(this.hCursor, this.hCursor + len);
+      this.hCursor += buffer.byteLength;
+    }
 
     return {
       err: constants.WASI_ESUCCESS,
@@ -161,5 +187,27 @@ class WgetDataDevice
 
   override close(): Promise<number> {
     return this.invalidate();
+  }
+
+  override async ioctl(request: number, buf?: Uint8Array): Promise<number> {
+    switch (request) {
+      case ioctlRequests.WGETGS: {
+        if (buf === undefined || buf.byteLength < 4)
+          return constants.WASI_ENOBUFS;
+
+        const __buf = new Int32Array(buf.buffer, buf.byteOffset);
+
+        __buf[0] = this.response.status;
+        return constants.WASI_ESUCCESS;
+      }
+      case ioctlRequests.WGETRB:
+        this.currentBuffer = bufferType.BODY;
+        return constants.WASI_ESUCCESS;
+      case ioctlRequests.WGETRH:
+        this.currentBuffer = bufferType.HEADERS;
+        return constants.WASI_ESUCCESS;
+      default:
+        return constants.WASI_EINVAL;
+    }
   }
 }
