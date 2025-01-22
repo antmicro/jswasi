e0dfd318bda1ba30d5a66fdbf41a3ae72b47f6fe
Krzysztof Juszczyk <kjuszczyk@antmicro.com>
2025-01-22 16:09:11 +0100

    [#71198] Add tests for `WebsocketDevice` class

--- tests/unit/websocket.test.ts
+++ tests/unit/websocket.test.ts
@@ -21,7 +21,7 @@ Object.assign(global, { WebSocket: require('ws') });
 jest.mock("../../src/filesystem/virtual-filesystem/devices/device-filesystem");
 jest.mock("ws");
 
-describe("Test Websocket Device Driver", () => {
+describe("Test WebsocketDeviceDriver", () => {
   let devfs: DeviceFilesystem;
   let websocketDeviceDriver: WebsocketDeviceDriver;
 
@@ -44,10 +44,11 @@ describe("Test Websocket Device Driver", () => {
   })
 
   afterEach(() => {
+    jest.clearAllMocks();
     jest.restoreAllMocks()
   });
 
-  test("Websocket device driver returns WebsocketDevice at minor 0 request", async () => {
+  test("WebsocketDeviceDriver returns WebsocketDevice at minor 0 request", async () => {
     const minor = 0;
 
     const { desc, err } = await websocketDeviceDriver.getDesc(minor, 0, 0n, 0n, vfs.CharacterDev);
@@ -55,14 +56,14 @@ describe("Test Websocket Device Driver", () => {
     expect(desc).toBeInstanceOf(WebsocketDevice);
   });
 
-  test("Websocket device driver doesn't return unexisting connection device", async () => {
+  test("WebsocketDeviceDriver doesn't return unexisting connection device", async () => {
     const minor = 1;
 
     const { err } = await websocketDeviceDriver.getDesc(minor, 0, 0n, 0n, vfs.CharacterDev);
     expect(err).toBe(constants.WASI_ENOENT);
   });
 
-  test("Websocket device driver properly manages conenction devices", async () => {
+  test("WebsocketDeviceDriver properly manages conenction devices", async () => {
     const connectionObjects: Record<number, WebSocketConnection> = {};
 
     for (var idx = 1; idx <= 3; idx++) {
@@ -70,7 +71,7 @@ describe("Test Websocket Device Driver", () => {
 
       Object.defineProperty(websocket, "readyState", {
         get() {
-          return websocket.OPEN;
+          return WebSocket.OPEN;
         }
       });
 
@@ -114,3 +115,108 @@ describe("Test Websocket Device Driver", () => {
     }
   });
 });
+
+describe("Test WebsocketDevice", () => {
+  let devfs: DeviceFilesystem;
+  let websocketDeviceDriver: WebsocketDeviceDriver;
+  // @ts-ignore
+  let websocketDevice: WebsocketDevice;
+
+  beforeAll(() => {
+    devfs = new DeviceFilesystem();
+    websocketDeviceDriver = new WebsocketDeviceDriver();
+  });
+
+  beforeEach(async () => {
+    await websocketDeviceDriver.initDriver({ devfs });
+    websocketDevice = new WebsocketDevice(0, 0n, 0n, vfs.CharacterDev, websocketDeviceDriver);
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    jest.restoreAllMocks();
+  });
+
+  test("WebsocketDevice is not terminal device", () => {
+    let istty = websocketDevice.isatty();
+    expect(istty).toBe(false);
+  })
+
+  test("WebsocketDevice should not create invalid connection devices", async () => {
+    jest.spyOn(WebSocket, "WebSocket").mockImplementationOnce(() => {
+      throw new Error("WebSocket open connection error");
+    });
+
+    let writeResult = await websocketDevice.write(new TextEncoder().encode("ws://some.link"));
+    expect(WebSocket).toBeCalledWith("ws://some.link");
+    expect(writeResult.err).toBe(constants.WASI_EINVAL)
+
+    let websocketMock = new WebSocket("ws://sus-websocket.link");
+    jest.spyOn(WebSocket, "WebSocket").mockImplementation(() => {
+      return websocketMock;
+    });
+
+    WebSocket.mockClear();
+
+    let writePromise = websocketDevice.write(new TextEncoder().encode("ws://some.link"));
+    const errorCallbackTrigger = async () => {
+      while (!websocketMock.onerror) {
+        await new Promise((resolve) => { setTimeout(() => resolve(null), 1) });
+      }
+      websocketMock.onerror();
+    };
+
+    await Promise.all([writePromise, errorCallbackTrigger()])
+    expect(WebSocket).toBeCalledWith("ws://some.link");
+    writeResult = await writePromise;
+    expect(writeResult.err).toBe(constants.WASI_ECONNREFUSED);
+
+    WebSocket.mockClear();
+
+    // const creationMock =
+    jest.spyOn(websocketDeviceDriver, "createConnectionDevice")
+      .mockReturnValue(new Promise((resolve) => resolve({
+        err: constants.WASI_EINVAL,
+        minor: undefined,
+      })))
+
+    writePromise = websocketDevice.write(new TextEncoder().encode("ws://some.link"));
+    const openCallbackTrigger = async () => {
+      while (!websocketMock.onopen) {
+        await new Promise((resolve) => { setTimeout(() => resolve(null), 1) });
+      }
+      websocketMock.onopen();
+    };
+
+    await Promise.all([writePromise, openCallbackTrigger()])
+    expect(WebSocket).toBeCalledWith("ws://some.link");
+    writeResult = await writePromise;
+    expect(writeResult.err).toBe(constants.WASI_EINVAL);
+
+    // creationMock.mockRestore();
+  })
+
+  test("WebsocketDevice opens websocket and returns correct minor device number", async () => {
+    const minor = 666;
+    const websocketMock = new WebSocket("ws://sus-websocket.link");
+    WebSocket.mockClear();
+
+    jest.spyOn(websocketDeviceDriver, "createConnectionDevice")
+      .mockReturnValue(new Promise((resolve) => resolve({
+        err: constants.WASI_ESUCCESS,
+        minor,
+      })))
+
+    const writePromise = websocketDevice.write(new TextEncoder().encode("ws://good.link"));
+    const openCallbackTrigger = async () => {
+      while (!websocketMock.onopen) {
+        await new Promise((resolve) => { setTimeout(() => resolve(null), 1) });
+      }
+      websocketMock.onopen();
+    };
+
+    await Promise.all([writePromise, openCallbackTrigger()])
+    expect(WebSocket).toBeCalledWith("ws://good.link");
+    expect(await writePromise).toStrictEqual({ err: constants.WASI_ESUCCESS, written: BigInt(minor) });
+  })
+})
