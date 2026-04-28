a270253d9ffd7d7c822e094c7e93411eb34889ca
Stanislaw Krowicki <skrowicki@internships.antmicro.com>
2026-04-28 14:11:44 +0200

    [#53124] Extend procfs functionalities
    Add /proc/pid/environ
    
    Add /proc/pid/stat
    
    Add /proc/meminfo
    
    Add /proc/pid/cwd

--- src/filesystem/proc-filesystem/proc-tree.ts
+++ src/filesystem/proc-filesystem/proc-tree.ts
@@ -107,6 +107,102 @@ class MountinfoFile extends AbstractProcFile {
   }
 }
 
+class MeminfoFile extends AbstractProcFile {
+  // kB in /proc/meminfo means kiB
+  private toKBWithPadding(val: number): string {
+    return `${(val / 1024).toFixed(0).toString()} kB`.padStart(10, " ");
+  }
+
+  read(): string {
+    const totalMemory = (performance as any).memory.jsHeapSizeLimit;
+    const usedMemory = (performance as any).memory.usedJSHeapSize;
+    const freeMemory = totalMemory - usedMemory;
+
+    let output = "";
+
+    output += `MemTotal:        ${this.toKBWithPadding(totalMemory)}\n`;
+    output += `MemFree:         ${this.toKBWithPadding(freeMemory)}\n`;
+    output += `MemAvailable:    ${this.toKBWithPadding(freeMemory)}\n`;
+
+    return output;
+  }
+}
+
+class StatusFile extends AbstractProcFile {
+  constructor(private pid: number) {
+    super();
+  }
+
+  read(): string {
+    const processInfo = processManager.processInfos[this.pid];
+
+    const cmd = processInfo.cmd.split("/");
+    const name = cmd[cmd.length - 1];
+
+    // Normally init system has pid of 1 and ppid of 0 (the scheduler)
+    // but for us init is at pid 0 and ppid null, which will be mapped to -1 here
+    const status =
+      `Name:\t${name}\n` +
+      `State:\tR (running)\n` +
+      `Pid:\t${this.pid}\n` +
+      `PPid:\t${processInfo.parentId ?? -1}\n`;
+
+    return status;
+  }
+}
+
+// This mimics proc_pid_stat file up to starttime
+// with many of the values hardcoded to placeholders
+class StatFile extends AbstractProcFile {
+  constructor(private pid: number) {
+    super();
+  }
+
+  read(): string {
+    const info = processManager.processInfos[this.pid];
+    const cmdPath = info.cmd.split("/");
+    const comm = cmdPath[cmdPath.length - 1];
+
+    const startTime = info.timestamp;
+
+    const currentTime = new Date().getTime();
+    const utime = currentTime - startTime;
+    // we do not differentiate between kernel time and user time,
+    // so these two are the same for now
+    const stime = utime;
+
+    let cutime = 0;
+    const cstime = cutime;
+
+    let tty = info.foreground;
+    let ttyNumber = 0;
+
+    if (tty !== null) {
+      ttyNumber |= tty.min & 0xff;
+      ttyNumber |= tty.maj << 8;
+      ttyNumber |= (tty.min & ~0xff) << 12;
+    }
+
+    return `${info.id} (${comm}) R ${info.parentId ?? -1} 0 0 ${ttyNumber} 0 0 0 0 0 0 ${utime} ${stime} ${cutime} ${cstime} -2 0 1 0 ${startTime}\n`;
+  }
+}
+
+class EnvironFile extends AbstractProcFile {
+  constructor(private pid: number) {
+    super();
+  }
+
+  read(): string {
+    const env = processManager.processInfos[this.pid].env;
+    let envString = "";
+
+    for (const [key, value] of Object.entries(env))
+      envString += `${key}=${value}\0`;
+
+    return envString;
+  }
+}
+
 class SelfSymlink extends AbstractProcSymlink {
   constructor(private pid: number) {
     super();
@@ -114,6 +210,16 @@ class SelfSymlink extends AbstractProcSymlink {
 
   read(): string {
     return this.pid.toString();
+  }
+}
+
+class CwdSymlink extends AbstractProcSymlink {
+  constructor(private pid: number) {
+    super();
+  }
+
+  read(): string {
+    return processManager.processInfos[this.pid].cwd;
   }
 }
 
@@ -132,6 +238,12 @@ export class TopLevelDirectory extends AbstractProcDirectory {
 
     for (const pid of Object.keys(processManager.processInfos))
       nodes[pid.toString()] = new ProcessDirectory(Number(pid));
+
+    if ((performance as any).memory !== undefined) {
+      // Add meminfo only for browsers that support memory.performance API
+      // (so Chromium-based)
+      nodes["meminfo"] = new MeminfoFile();
+    }
 
     return {
       err: constants.WASI_ESUCCESS,
@@ -157,6 +269,14 @@ export class TopLevelDirectory extends AbstractProcDirectory {
         err: constants.WASI_ESUCCESS,
         node: new SelfSymlink(this.pid),
       };
+    } else if (
+      name === "meminfo" &&
+      (performance as any).memory !== undefined
+    ) {
+      return {
+        err: constants.WASI_ESUCCESS,
+        node: new MeminfoFile(),
+      };
     }
     return {
       err: constants.WASI_ENOENT,
@@ -168,6 +288,10 @@ export class TopLevelDirectory extends AbstractProcDirectory {
 class ProcessDirectory extends AbstractProcDirectory implements ProcNode {
   static specialNodes: Record<string, new (pid: number) => ProcNode> = {
     mountinfo: MountinfoFile,
+    status: StatusFile,
+    environ: EnvironFile,
+    stat: StatFile,
+    cwd: CwdSymlink,
   };
 
   listNodes(): { err: number; nodes: Record<string, ProcNode> } {
@@ -190,6 +314,26 @@ class ProcessDirectory extends AbstractProcDirectory implements ProcNode {
         return {
           err: constants.WASI_ESUCCESS,
           node: new MountinfoFile(),
+        };
+      case "status":
+        return {
+          err: constants.WASI_ESUCCESS,
+          node: new StatusFile(this.pid),
+        };
+      case "environ":
+        return {
+          err: constants.WASI_ESUCCESS,
+          node: new EnvironFile(this.pid),
+        };
+      case "stat":
+        return {
+          err: constants.WASI_ESUCCESS,
+          node: new StatFile(this.pid),
+        };
+      case "cwd":
+        return {
+          err: constants.WASI_ESUCCESS,
+          node: new CwdSymlink(this.pid),
         };
       default:
         return {
